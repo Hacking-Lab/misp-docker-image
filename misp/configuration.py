bf3d253961047780fc9459b26ae11ad9e2a31e11
Marius Zindel <marius.zindel@hotmail.com>
2021-11-15 12:58:33 +0100

    update event import

--- misp/configuration.py
+++ misp/configuration.py
@@ -1,12 +1,14 @@
 
 import json
+from os import execlp, path
 from pymisp import ExpandedPyMISP, MISPUser, MISPServer, PyMISP, MISPOrganisation, MISPEvent
 import time
 import subprocess
 import uuid
 
-misp_url = "http://localhost/"
-misp_key = subprocess.getoutput("/var/www/MISP/app/Console/cake user change_authkey 1 | cut -d ':' -f 2 | cut -d ' ' -f 2")
+misp_url = "http://172.16.137.132/"
+#misp_key = subprocess.getoutput("/var/www/MISP/app/Console/cake user change_authkey 1 | cut -d ':' -f 2 | cut -d ' ' -f 2")
+misp_key = "wK4t1jDRa9u9OKEz2TOPtvGv8t9i93vCbxEB3MCp"
 misp_verifycert = False
 
 default_nickname = "investigator"
@@ -44,6 +46,15 @@ def createUsers():
         user.change_pw = 0
         misp.add_user(user, pythonify=True)
 
+def createUser():
+    user = MISPUser()
+    user.email = default_nickname + '@misp-lab99.com'
+    user.org_id = 3 #offset -> org 1 = admin org
+    user.role_id = 3
+    user.password ="compass"
+    user.change_pw = 0
+    print(misp.add_user(user, pythonify=True))
+
 # import MISP events from export File
 def importEventsFromFile(path):
     with open(path, 'r') as f:
@@ -51,9 +62,55 @@ def importEventsFromFile(path):
             event = json.loads(e)
     
     for i in range(len(event['response'])):
+        event['response'][i]['Event']['uuid'] = ""
+        event['response'][i]['Event']['Attribute'][0]['uuid'] = ""
         misp.add_event(event['response'][i])
+
+
+
+# import events for every lab
+def importEvents():
+    for lab in range(labs_count):
+        current_lab = 'Lab_' + str(lab + 1)
+
+        # open json file with events
+        try:
+            with open (current_lab + '.json', 'r') as f:
+                for e in f:
+                    events = json.loads(e)
+                print('found file ' + current_lab)
+        except:
+            print('cant find file')
+            continue
+
+        # find correct lab org uuid
+        org = misp.get_organisation(lab + 2)
+        org_id = org['Organisation']['id']
+        org_name = org ['Organisation']['name']
+        org_uuid = org['Organisation']['uuid']
+        
+        # edit file
+        for event in range(len(events['response'])):
+            # remove event uuid
+            events['response'][event]['Event']['uuid'] = ""
+            events['response'][event]['Event']['orgc_id'] = org_id
+            events['response'][event]['Event']['org_id'] = org_id
+            
+            # edit org
+            events['response'][event]['Event']['Org']['id'] = org_id
+            events['response'][event]['Event']['Org']['name'] = org_name
+            events['response'][event]['Event']['Org']['uuid'] = org_uuid
+            # edit orgC
+            events['response'][event]['Event']['Orgc']['id'] = org_id
+            events['response'][event]['Event']['Orgc']['name'] = org_name
+            events['response'][event]['Event']['Orgc']['uuid'] = org_uuid
+
+    # upload file
+            print(misp.add_event(events['response'][event]))
+
 
 setServerSettings()
 createOrg()
 createUsers()
-importEventsFromFile('./misp.json')
+importEvents()
+# createUser()
