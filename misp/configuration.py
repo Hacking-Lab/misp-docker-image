b60923a8f1e41fc92dace83ea9b979d4d81233c8
Marius Zindel <marius.zindel@hotmail.com>
2021-11-16 17:35:53 +0100

    update config for admin each lab

--- misp/configuration.py
+++ misp/configuration.py
@@ -11,22 +11,25 @@ misp_verifycert = False
 
 default_nickname = "investigator"
 default_pw = "compass"
+admin_nickname = "admin"
+admin_pw = "lab-admin"
+
 labs_count = 10
 
 def getVitalSigns():
     print("vital")
-    r = requests.get('http://172.16.137.132/users/login')
+    r = requests.get('http://localhost/users/login')
     print("MISP Status Code " + str(r.status_code))
     #print("MISP Reply" + r.text)
 
 
-def getKey():
-    global misp_key
-    misp_key = subprocess.getoutput("/var/www/MISP/app/Console/cake Admin change_authkey admin@admin.test | sed '1d'")
-    misp_key = subprocess.getoutput("/var/www/MISP/app/Console/cake user change_authkey admin@admin.test | cut -d ':' -f 2 | cut -d ' ' -f 2")
+def getKey(email):
+    misp_key = subprocess.getoutput("/var/www/MISP/app/Console/cake Admin change_authkey "+ email +" | sed '1d'")
+    misp_key = subprocess.getoutput("/var/www/MISP/app/Console/cake user change_authkey "+ email +" | cut -d ':' -f 2 | cut -d ' ' -f 2")
     print("Your MISP admin key is: " + misp_key)
+    return misp_key
 
-def getInstance():
+def getInstance(misp_key):
     global misp 
     print("Instance got MISP key:" + misp_key)
     misp = PyMISP(misp_url, misp_key, misp_verifycert)
@@ -49,29 +52,44 @@ def createOrg():
         misp.add_organisation(org, pythonify=True)
 
 # Create users
-def createUsers():
+def createLabUsers():
     for i in range(labs_count):
         user = MISPUser()
         user.email = default_nickname + '@misp-lab' + str(i+1) + '.com'
         user.org_id = i+2 #offset -> org 1 = admin org
         user.role_id = 3
-        user.password ="compass"
+        user.password = default_pw
         user.change_pw = 0
         misp.add_user(user, pythonify=True)
+
+def createLabAdmins():
+    global admin_auth_keys
+    admin_auth_keys = []
+    for i in range(labs_count):
+        user = MISPUser()
+        user.email = admin_nickname + '@misp-lab' + str(i+1) + '.com'
+        user.org_id = i+2 #offset -> org 1 = admin org
+        user.role_id = 1
+        user.password = admin_pw
+        user.change_pw = 0
+        misp.add_user(user, pythonify=True)
+        admin_auth_keys.append(getKey(user.email))
 
 # import events for every lab
 def importEvents():
     for lab in range(labs_count):
+        
+        misp_instance = PyMISP(misp_url, admin_auth_keys.pop(0), misp_verifycert)
         current_lab = 'Lab_' + str(lab + 1)
 
         # open json file with events
         try:
-            with open ('/' + current_lab + '.json', 'r') as f:
+            with open ('./' + current_lab + '.json', 'r') as f:
                 for e in f:
                     events = json.loads(e)
                 print('found file ' + current_lab)
         except:
-            print('cant find file' + current_lab)
+            print('cant find file ' + current_lab)
             continue
 
         # find correct lab org uuid
@@ -97,7 +115,7 @@ def importEvents():
             events['response'][event]['Event']['Orgc']['uuid'] = org_uuid
 
     # upload file
-            print(misp.add_event(events['response'][event]))
+            print(misp_instance.add_event(events['response'][event]))
 
 
 x = True
@@ -105,8 +123,8 @@ while x:
     try:
         time.sleep(10)
         getVitalSigns()
-        getKey()
-        getInstance()
+        misp_key = getKey('admin@admin.test')
+        getInstance(misp_key)
         misp.update_object_templates()
         x = False
     except KeyboardInterrupt:
@@ -118,6 +136,7 @@ print("MISP instance is running, init lab env")
 time.sleep(5)
 setServerSettings()
 createOrg()
-createUsers()
+createLabUsers()
+createLabAdmins()
 importEvents()
 os.system("cp /logo.png /var/www/MISP/app/webroot/img/custom/logo.png")
