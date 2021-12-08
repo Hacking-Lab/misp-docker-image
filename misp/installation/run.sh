a9c873eb5794a4695485dfb51a779a9094c4bd95
Marius Zindel <marius.zindel@hotmail.com>
2021-12-08 09:40:15 +0100

    update supervisor configuration

--- misp/installation/run.sh
+++ misp/installation/run.sh
@@ -14,8 +14,6 @@ set -e
 
 if [ -r /.firstboot.tmp ]; then
         sed -i "3i loglevel = debug" /etc/supervisor/conf.d/supervisord.conf
-
-        echo -e '\n[program:proxy]\ncommand=/bin/bash -c "python3 /configuration.py"\nuser = www-data\nstartsecs = 20\nautorestart = false\nstartretries = 1\n' >> /etc/supervisor/conf.d/supervisord.conf
         echo "Container started for the fist time. Setup might time a few minutes. Please wait..."
         echo "(Details are logged in /tmp/install.log)"
         export DEBIAN_FRONTEND=noninteractive
